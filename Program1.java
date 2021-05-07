86174ced4ef77e373190bfc85497d9fbff44e6a1
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2021-05-07 11:37:44 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,15 @@
+
+/****************************************************************
+
+  Basic Code to display a message in the output screen
+	  
+*****************************************************************/
+
+
+class Main
+{
+  public static void main(String args[])
+  {
+    System.out.println("Hi Dev");
+  }
+}
